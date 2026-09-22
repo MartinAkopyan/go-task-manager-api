9353b85c762ff992eadcae9efbfebbebb38a2ecd
mrtn <martinakopyan34@gmail.com>
2026-09-22 20:35:37 +0300

    Add DB connection via pgxpool with Ping check

--- db.go
+++ db.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func DBPool(ctx context.Context) (*pgxpool.Pool, error) {
+	dsn := os.Getenv("DATABASE_URL")
+	pool, err := pgxpool.New(ctx, dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = pool.Ping(ctx)
+
+	if err != nil {
+		pool.Close()
+		return nil, err
+	}
+
+
+	return pool, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
-package main 
+package main
+
+import (
+	"context"
+	"log"
+)
 
 func main() {
+	ctx := context.Background()
+
+	pool, err := DBPool(ctx)
+
+	if err != nil {
+		log.Fatalf("DB connection error: %v", err)
+	}
+
+	defer pool.Close()
+
+	log.Println("Succesfull connection")
 
 }
