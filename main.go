19d389bcb8803968e765e286d9bf02f278635f4b
mrtn <martinakopyan34@gmail.com>
2026-09-23 20:09:49 +0300

    Add docker-compose.yml

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
 
-	srv := &http.Server{Addr: ":8080", Handler: r}
+	srv := &http.Server{Addr: ":" + os.Getenv("PORT"), Handler: r}
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
@@ -38,10 +38,10 @@ func main() {
 	<-signalChan
 	log.Println("shutting down...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	if err := srv.Shutdown(ctx); err != nil {
+	if err := srv.Shutdown(shutdownCtx); err != nil {
 		log.Printf("shutdown error: %v", err)
 	}
 	log.Println("server stopped")
